1bd6924c6972f08a3b592a6c526d94cc9f72cc4f
mikeezvz <mike.zogheib@outlook.com>
2025-04-15 11:29:41 +0200

    style: grid layout for header created

--- quoties-fe/src/pages/index.js
+++ quoties-fe/src/pages/index.js
@@ -4,13 +4,25 @@ import { Geist, Geist_Mono } from "next/font/google";
 export default function Home() {
   return (
     <>
-    <div>
-      dffaf
-    </div>
-        <div>
-        dffaf
+      <div className="grid grid-cols-4 gap-4 w-9/10">
+        <div className="col-span-2">
+          Quoties
+        </div>
+
+        <div className="flex gap-40">
+          <div>aa</div>
+          <div>aa</div>
+        </div>  
+
+
+        <div className="flex justify-end">
+          Aa
+        </div>
+        
+        <div></div>
+    
       </div>
-      </>
+    </>
     
   );
 }
