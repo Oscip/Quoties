4c1c2118a0161bdff19184a8f72d3866284d9e15
mikeezvz <mike.zogheib@outlook.com>
2025-05-20 09:04:42 +0200

    feature: button to pick random word
    button to pick random word from API, with effects. API function for button not yet implemented.

--- quoties-fe/src/pages/index.js
+++ quoties-fe/src/pages/index.js
@@ -2,21 +2,30 @@ import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 
 export default function Home() {
+
   return (
     <>
-      <div className="grid grid-cols-4 gap-4 w-9/10">
-        <div className="col-span-2">
+      <div className="grid grid-cols-5 gap-4 w-9/10 place-items-center">
+        <div className="col-span-1">
           Quoties
         </div>
+        <div></div>
+        <div className="flex gap-40 items-center justify-center">
 
-        <div className="flex gap-40">
-          <div>aa</div>
-          <div>aa</div>
+          <div className="whitespace-nowrap">
+            Random Pick
+          </div>
+          <div>
+            <button className="p-1 rounded hover:scale-125 transition-transform">
+            <Image src="/item_box.webp" alt="Beispiel" width={70} height={70} />
+            </button>
+          </div>
+
         </div>  
 
 
         <div className="flex justify-end">
-          Aa
+          
         </div>
         
         <div></div>
